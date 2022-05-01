78fe874a858ab6db36fbbd4396d4b1fe40c8fb7b
fangqk1991 <me@fangqk.com>
2022-05-01 19:22:50 +0800

    (models): +Group interfaces and classes

--- src/models/extensions/_CommonPermission.ts
+++ src/models/extensions/_CommonPermission.ts
@@ -4,4 +4,10 @@ export class _CommonPermission extends __CommonPermission {
   public constructor() {
     super()
   }
+
+  public static async permissionsForGroupId<T extends _CommonPermission>(this: { new (): T }, groupId: string) {
+    const searcher = new this().fc_searcher()
+    searcher.processor().addConditionKV('group_id', groupId)
+    return searcher.queryFeeds()
+  }
 }
